2223cf2e8c70f5a00ee0bb72b695ee20d1587fae
ervanrahadian <ervanrahadian@gmail.com>
2021-03-09 22:25:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: null,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  getItems() {
+    fetch("https://dekontaminasi.com/api/id/covid19/hospitals")
+      .then((results) => results.json())
+      .then((results) => this.setState({ items: results }));
+  }
+
+  searchSpace = (event) => {
+    let keyword = event.target.value;
+    this.setState({ search: keyword });
+  };
+
+  render() {
+    const showAlert = (data) => {
+      alert(
+        "Rumah Sakit : " +
+          data.name +
+          "\n" +
+          "Alamat : " +
+          data.address +
+          "\n" +
+          "Wilayah : " +
+          data.region +
+          "\n" +
+          "Telepon : " +
+          data.phone +
+          "\n" +
+          "Provinsi : " +
+          data.province
+      );
+    };
+
+    const items = this.state.items
+      .filter((data) => {
+        if (this.state.search == null) return data;
+        else if (
+          data.name.toLowerCase().includes(this.state.search.toLowerCase())
+        ) {
+          return data;
+        }
+      })
+      .map((data) => {
+        return (
+          <tr onClick={() => showAlert(data)} key={data.name}>
+            <td>{data.name}</td>
+            <td>{data.address}</td>
+            <td>{data.region}</td>
+            <td>{data.phone}</td>
+            <td>{data.province}</td>
+          </tr>
+        );
+      });
+
+    return (
+      <>
+        {/* Navbar */}
+        <div class="navbar">
+          {/* Title */}
+          <div class="title">
+            <p>Daftar Rumah Sakit</p>
+          </div>
+
+          {/* Search Bar */}
+          <div class="search">
+            <input
+              type="text"
+              placeholder="Cari Rumah Sakit"
+              onChange={(e) => this.searchSpace(e)}
+            />
+          </div>
+        </div>
+
+        {/* Table */}
+        <div class="table">
+          <Table responsive="sm">
+            {/* Head Table */}
+            <thead>
+              <tr>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Wilayah</th>
+                <th>Telepon</th>
+                <th>Provinsi</th>
+              </tr>
+            </thead>
+
+            {/* Body Table */}
+            <tbody style={{ cursor: "pointer" }}>{items}</tbody>
+          </Table>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
